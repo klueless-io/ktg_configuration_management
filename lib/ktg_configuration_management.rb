e773a8f8bc5752780bb33a6ae85491606024fb45
David <david.cruwys@bugcrowd.com>
2020-01-27 12:48:37 +1100

    Initial Commit

---
+++ lib/ktg_configuration_management.rb
@@ -0,0 +1,6 @@
+require "ktg_configuration_management/version"
+
+module KtgConfigurationManagement
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ktg_configuration_management/version.rb
@@ -0,0 +1,3 @@
+module KtgConfigurationManagement
+  VERSION = "0.1.0"
+end
